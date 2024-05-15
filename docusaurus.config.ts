d00b2290c6c0e975106f643fc871f1ac20036e95
Sean Lin <sean.enylin@gmail.com>
2024-05-15 22:51:02 +0800

    update docusaurus config to set up navbar and footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -2,31 +2,38 @@ import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
+const organizationName = 'project-mars-tw';
+const projectName = 'mars-dictionary';
+const baseUrl = `/${projectName}`;
+const githubUrl = `https://github.com/${organizationName}/${projectName}`
+
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: '火星辭典',
+  tagline: '火星辭典',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://project-mars-tw.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName, // Usually your GitHub org/user name.
+  projectName, // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
+
+  trailingSlash: true,
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'zh-Hant',
+    locales: ['zh-Hant'],
   },
 
   presets: [
@@ -38,14 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            `${githubUrl}/tree/main/`,
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -58,52 +58,63 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: '火星辭典',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: '火星辭典 Logo',
+        src: 'img/logo192.png',
       },
       items: [
         {
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: '辭典',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
+          type: 'search',
           position: 'right',
         },
+        {
+          href: githubUrl,
+          label: 'GitHub',
+          position: 'right',
+        }
       ],
     },
     footer: {
       style: 'dark',
       links: [
         {
-          title: 'Docs',
+          title: '火星辭典',
           items: [
             {
-              label: 'Tutorial',
-              to: '/docs/intro',
+              label: '首頁',
+              to: '/',
             },
           ],
         },
         {
-          title: 'Community',
+          title: '火星計畫',
           items: [
             {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              label: '官方網站',
+              href: 'https://www.projectmars.info',
             },
             {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
+              label: '火星人試驗',
+              href: 'https://www.projectmars.org',
             },
             {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
+              label: '火星會員',
+              href: 'https://www.projectmars.club',
+            },
+            {
+              label: '火星計畫商城',
+              href: 'https://www.projectmars.shop',
+            },
+            {
+              label: '火星推進器商城',
+              href: 'https://www.projectmars.app',
             },
           ],
         },
@@ -111,23 +122,34 @@ const config: Config = {
           title: 'More',
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
               label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              href: githubUrl,
             },
           ],
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © ${new Date().getFullYear()} Project Mars Co., Ltd.`,
     },
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
     },
   } satisfies Preset.ThemeConfig,
+  themes: [
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+        // For Docs using Chinese, The `language` is recommended to set to:
+        // ```
+        language: ['zh', 'en'],
+        // ```
+      }),
+    ],
+  ],
 };
 
 export default config;
